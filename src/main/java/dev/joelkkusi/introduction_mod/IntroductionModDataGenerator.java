54bf6599c5f7d5a8e08ca138b6f39ec22f962d3a
Joel Kusi <joelkkusi@gmail.com>
2024-08-04 01:47:11 +0200

    Initial commit

---
+++ src/main/java/dev/joelkkusi/introduction_mod/IntroductionModDataGenerator.java
@@ -0,0 +1,11 @@
+package dev.joelkkusi.introduction_mod;
+
+import net.fabricmc.fabric.api.datagen.v1.DataGeneratorEntrypoint;
+import net.fabricmc.fabric.api.datagen.v1.FabricDataGenerator;
+
+public class IntroductionModDataGenerator implements DataGeneratorEntrypoint {
+	@Override
+	public void onInitializeDataGenerator(FabricDataGenerator fabricDataGenerator) {
+
+	}
+}
